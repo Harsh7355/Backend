b26ea02f8abc5a9df07a6b67a63637ce072fe1e5
Harsh <youremail@example.com>
2025-06-09 21:05:25 +0530

    updating the routes also

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router =express.Router();
-import {registeruser,loginuser,logoutuser,refreshaccesstoken} from '../controllers/user.controller.js'
+import {registeruser,loginuser,logoutuser,refreshaccesstoken,changecurrentpassword,getcurrentuser,updateaccountdetails,avatarupdated,coverimageupdate,getuserchannelprofile,getwatchhistory} from '../controllers/user.controller.js'
 import {upload} from "../middlewares/multer.middleware.js"
 import jwtverify from '../middlewares/auth.middleware.js'
+import multer from 'multer';
 
 router.route('/register').post(  
     upload.fields([
@@ -15,6 +16,14 @@ router.route('/login').post(loginuser)
 
 router.route('/logout').post(jwtverify,logoutuser)
 router.route('/refreshtoekn').post(refreshaccesstoken)
+router.route('/changepassword').post(jwtverify,changecurrentpassword)
+router.route('/curruser').post(jwtverify,getcurrentuser)
+router.route('/updateaccountdetails').patch(jwtverify,updateaccountdetails)
+router.route('/avatarupdated').patch(jwtverify,upload.single("avatar"),avatarupdated)
+router.route('/coverimageupdate').post(jwtverify,upload.single("coverimage"),coverimageupdate)
+// because params
+router.route('/c/:username').get(jwtverify,getuserchannelprofile)
+router.route('/history').get(jwtverify,getwatchhistory)
 
 export default router;
 
